465c22884896b2e96c783a2e94c9d839d158fbd9
hiruenagi <72330441+hiruenagi@users.noreply.github.com>
2022-06-10 01:11:09 +0800

    新增

--- Assets/Code/Remover.cs
+++ Assets/Code/Remover.cs
@@ -5,6 +5,30 @@ using System.Collections;
 public class Remover : MonoBehaviour
 {
 	public GameObject splash;
+	public PlayerHealth playerHealth;
+
+	void Awake()
+	{
+		// Setting up the reference.
+		playerHealth = GameObject.Find("Hero").GetComponent<PlayerHealth>();
+	}
+	void Update()
+	{
+		if (playerHealth.health <= 0)
+		{
+			GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Cinemachine.CinemachineBrain>().enabled = false;
+
+			//防止英雄销毁后再去获取位置出现NullReferenceException
+			//if (GameObject.Find("UI_HealthBar").activeSelf)
+			//{
+			//	GameObject.Find("UI_HealthBar").SetActive(false);
+			//}
+
+
+
+			StartCoroutine("ReloadGame");
+		}
+	}
 
 
 	void OnTriggerEnter2D(Collider2D col)
@@ -16,10 +40,10 @@ public class Remover : MonoBehaviour
 			GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Cinemachine.CinemachineBrain>().enabled = false;
 
 			//防止英雄销毁后再去获取位置出现NullReferenceException
-			if(GameObject.Find("UI_HealthBar").activeSelf)
+			/*if(GameObject.Find("UI_HealthBar").activeSelf)
 			{
 				GameObject.Find("UI_HealthBar").SetActive(false);
-			}
+			}*/
 
 			
 			Instantiate(splash, col.transform.position, transform.rotation);
